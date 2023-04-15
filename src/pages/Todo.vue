06f586db0eba6e451a3cbbfee7958c76c56ac7d9
Lendon Ato <lendon.n.ato@gmail.com>
2023-04-15 19:13:40 +0800

    Added qlist to todo page

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,6 +1,16 @@
 <template>
-  <q-page>
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white">
+      <q-item v-ripple>
+        <q-item-section avatar>
+          <q-checkbox
+                      v-model="color"
+                      color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Teal</q-item-label>
+        </q-item-section>
+      </q-item>    </q-list>
   </q-page>
 </template>
 

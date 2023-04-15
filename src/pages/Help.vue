6e3bc6df31c8d43cc009ac2fffa836d1e49c0f58
Lendon Ato <lendon.n.ato@gmail.com>
2023-04-15 19:08:54 +0800

    Add content to Help page

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-page>
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
   </q-page>
 </template>
 
